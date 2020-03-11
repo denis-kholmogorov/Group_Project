2a9820d4c47e51b56d68e38dedd1f5904b1c7c76
Denis <dendiesel88@yandex.ru>
2020-03-11 17:10:46 +0300

    Add SecurityConfig

---
+++ src/main/java/project/controllers/StarterController.java
@@ -0,0 +1,16 @@
+package project.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+/**
+ * Данный контроллер просто пустышка на url("/") */
+@Controller
+public class StarterController {
+
+    @RequestMapping("/")
+    public String index(Model model)
+    {
+        return "hello";
+    }
+}
--- src/main/java/project/repositories/PostLikeRepository.java
+++ src/main/java/project/repositories/PostLikeRepository.java
@@ -1,8 +1,9 @@
 package project.repositories;
 
+import project.models.PostLike;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostLikeRepository extends CrudRepository<PostLikeRepository, Integer> {
+public interface PostLikeRepository extends CrudRepository<PostLike, Integer> {
 }
