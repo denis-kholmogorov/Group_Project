6a7eb87619486954b5fedf872aa662cc4aabaa61
Roman Ambartsumyan <r9854334307@mail.ru>
2020-03-16 10:08:45 +0300

    Repositories are corrected

--- src/main/java/project/repositories/PersonRepository.java
+++ src/main/java/project/repositories/PersonRepository.java
@@ -8,7 +8,7 @@ import project.models.Person;
 import java.util.Optional;
 
 @Repository
-public interface PersonRepository extends CrudRepository<Person, Long> {
+public interface PersonRepository extends CrudRepository<Person, Integer> {
 
      @Query("Select p from Person p where p.email = :email")
      Optional<Person> findPersonByEmail(String email);
