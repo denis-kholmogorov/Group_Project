e297be14751d3f2acb69a6564ee99e28d0f5eb0c
Sergey <cer1990@gmail.com>
2020-03-25 18:40:47 +0300

    added language functionality

--- src/main/java/project/handlerExceptions/EntityValidationException.java
+++
@@ -1,8 +0,0 @@
-package project.handlerExceptions;
-
-public class EntityValidationException extends Exception {
-
-    public <T> EntityValidationException(Class<T> entityClass, String message) {
-        super("Error on validation entity: " + entityClass + "\n" + message);
-    }
-}
