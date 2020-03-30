2a9a6a508296a448732c076b0b1396578a678378
Sergey <cer1990@gmail.com>
2020-03-30 18:46:30 +0300

    changed strategy GenerationType

--- src/main/java/project/models/Image.java
+++ src/main/java/project/models/Image.java
@@ -13,7 +13,7 @@ import javax.persistence.Lob;
 public class Image {
 
     @Id
-    @GeneratedValue(strategy =  GenerationType.AUTO)
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private Integer id;
 
     @Lob
