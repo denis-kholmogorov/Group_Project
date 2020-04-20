699876d4e4695fcd21a48ef6cffa1ff444b3c211
girin_ilia <ilyxa.girin@mail.ru>
2020-04-20 14:24:24 +0300

    UsersController tests added (but not all)

--- src/test/java/project/controllers/rest/ApiAuthControllerTest.java
+++ src/test/java/project/controllers/rest/ApiAuthControllerTest.java
@@ -9,40 +9,52 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import project.dto.requestDto.LoginRequestDto;
+import project.security.TokenProvider;
+import project.services.PersonService;
 
 import static org.hamcrest.Matchers.*;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@Sql(value = {"/insert.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(value = {"/delete.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("dev")
+@TestPropertySource("/application-test.properties")
 class ApiAuthControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     private static final ObjectMapper om = new ObjectMapper();
+    private final String token2;
+
+    @Autowired
+    public ApiAuthControllerTest(TokenProvider tokenProvider, PersonService personService) {
+        token2 = tokenProvider.createToken("test2@mail.ru");
+    }
 
     @Test
     void login() throws Exception {
-        LoginRequestDto dto = new LoginRequestDto("ilyxa043@gmail.com", "qweasdzxc");
+        LoginRequestDto dto = new LoginRequestDto("test2@mail.ru", "123123123");
         String json = om.writeValueAsString(dto);
         System.out.println(json);
 
         mockMvc.perform(post("/api/v1/auth/login")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(json))
+                .content(json)
+                .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.data.id", is(1)));
+                .andExpect(jsonPath("$.data.id", is(2)));
     }
 
     @Test
@@ -50,12 +62,9 @@ class ApiAuthControllerTest {
     void logout() throws Exception {
         mockMvc.perform(post("/api/v1/auth/logout")
                 .accept(MediaType.APPLICATION_JSON)
-                .header("Authorization",
-                        "eyJhbGciOiJIUzI1NiJ9" +
-                        ".eyJzdWIiOiJpbHl4YTA0M0BnbWFpbC5jb20iLCJpYXQiOjE1ODcxMTY0NzYsImV4cCI6MTU5MDcxNjQ3Nn0" +
-                        ".puucO74gPBqV5OOFhBbafLnLFWrDC6igrGZdeBKJ8NQ"))
+                .header("Authorization", token2))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$data.message", is("ok")));
+                .andExpect(jsonPath("$.data.message", is("ok")));
     }
 }
