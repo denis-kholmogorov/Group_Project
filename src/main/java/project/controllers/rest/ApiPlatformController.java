2e53f69160f3e2ab9411d6300a276f3e27760483
Sergey-PC <cer1990@gmail.com>
2020-04-20 18:10:13 +0300

    refactoring languages service

--- src/main/java/project/controllers/rest/ApiPlatformController.java
+++ src/main/java/project/controllers/rest/ApiPlatformController.java
@@ -1,6 +1,7 @@
 package project.controllers.rest;
 
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,30 +15,26 @@ import project.services.PlatformService;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.PositiveOrZero;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/v1/platform/")
-@AllArgsConstructor
 @Validated
 public class ApiPlatformController {
 
+    @Autowired
     private PlatformService platformService;
 
     @GetMapping("languages")
-    ResponseEntity<?> languages(@RequestParam(required = false) String name,
+    ResponseEntity<?> languages(@RequestParam(required = false) String language,
                                 @RequestParam(required = false, defaultValue = "0") @PositiveOrZero Integer offset,
                                 @RequestParam(required = false, defaultValue = "20") @Positive @Max(20) Integer itemPerPage) {
-        long total;
-        List<Language> list;
-        if (name == null || name.isEmpty()) {
-            total = platformService.languageCount();
-            list = platformService.getLanguages(offset, itemPerPage);
+        Page<Language> page;
+        if (language == null || language.isEmpty()) {
+            page = platformService.getLanguages(offset, itemPerPage);
         } else {
-            total = platformService.languageCount(name);
-            list = platformService.getLanguages(name, offset, itemPerPage);
+            page = platformService.getLanguages(language, offset, itemPerPage);
         }
 
-        return ResponseEntity.ok(new ListResponseDto<>(total, offset, itemPerPage, list));
+        return ResponseEntity.ok(new ListResponseDto<>(page.getTotalElements(), offset, itemPerPage, page.getContent()));
     }
 }
--- src/main/java/project/repositories/LanguageRepository.java
+++ src/main/java/project/repositories/LanguageRepository.java
@@ -1,16 +1,13 @@
 package project.repositories;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import project.models.Language;
 
-import java.util.List;
-
 @Repository
 public interface LanguageRepository extends JpaRepository<Language, Integer> {
 
-    List<Language> findByLanguageContaining(String language, Pageable pageable);
-
-    long countByLanguageContaining(String language);
+    Page<Language> findByLanguageContaining(String language, Pageable pageable);
 }
