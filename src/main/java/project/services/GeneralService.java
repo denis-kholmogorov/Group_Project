c2dc23dfb15f236f36cb7fca14edbcdb76c5f534
Sergey-PC <cer1990@gmail.com>
2020-04-21 20:35:12 +0300

    refactor storage service

--- src/main/java/project/services/GeneralService.java
+++ src/main/java/project/services/GeneralService.java
@@ -2,6 +2,7 @@ package project.services;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import project.handlerExceptions.EntityNotFoundException;
 import project.models.Image;
 import project.repositories.ImageRepository;
 
@@ -19,15 +20,17 @@ public class GeneralService {
         return imageSaved.getId();
     }
 
-    public Image findImage(Integer id) {
-        return imageRepository.findById(id).orElse(null);
+    public Image findImage(Integer id) throws EntityNotFoundException {
+        return imageRepository.findById(id).orElseThrow(
+            () -> new EntityNotFoundException("Can't find entity with id: " + id)
+        );
     }
 
     public void deleteImage(Integer id) {
         imageRepository.deleteById(id);
     }
 
-    public void updateImage(byte[] arrayImage, String type, Integer id) {
+    public void updateImage(byte[] arrayImage, String type, Integer id) throws EntityNotFoundException {
         Image image = findImage(id);
         image.setImage(arrayImage);
         image.setType(type);
